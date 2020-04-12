bb3080e3d75421df6cff1b503134124422a1db7e
b2t5OmqxV <tatuyaizumi7@gmail.com>
2020-04-12 08:25:40 +0000

    [fix]

--- db/migrate/20190109232832_add_introduction_to_users.rb
+++ db/migrate/20190109232832_add_introduction_to_users.rb
@@ -1,7 +1,7 @@
 class AddIntroductionToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :introduction, :text
-    add_colume :users, :profile_image_id, :string 
+    add_column :users, :profile_image_id, :string
 
   end
 end
